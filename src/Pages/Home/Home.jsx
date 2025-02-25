17b56cb245895e1536201b673df6d9e1eadc3557
Rohit <gaikwadr575@gmail.com>
2025-02-25 14:59:28 +0530

    testing netlify

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,15 @@ import React from "react";
 import { styles } from "./styles";
 
 const Home = () => {
-  return <styles.homeContainer>Home</styles.homeContainer>;
+  return (
+    <styles.homeContainer>
+      <a href="/">Home</a>
+      <br />
+      <a href="/register">Register</a>
+      <br />
+      <a href="/login">Login</a>
+    </styles.homeContainer>
+  );
 };
 
 export default Home;
